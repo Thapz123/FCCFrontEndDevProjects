3fda7b5925c2ae8649bf6daebf18223076335f58
Thapelo Sebolai <thapz.sebolai@gmail.com>
2016-05-08 11:39:27 +0200

    Completed wikiviewer

--- wikipedia-viewer-fcc/js/index.js
+++ wikipedia-viewer-fcc/js/index.js
@@ -1,18 +1,43 @@
 function getRequest() {
-  $.ajax({
-    url: "http://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srwhat=text&srsearch=hello",
-    dataType: jsonp,
-    success: function(data) {
-      console.log(data);
-    },
-    error: function() {
-      console.log("error");
-    }
+    var text = document.getElementById("text").value;
+    var display = document.getElementById("display");
+    display.innerHTML="";
+    $.ajax({
+        url: "http://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srwhat=text&srsearch=" + text,
+        dataType: "jsonp",
+        success: function(response) {
+            response.query.search.forEach(function(curr) {
+                //console.log(curr);
+                var searchItem = document.createElement('div');
+                searchItem.className='display';
+                var href= "https://en.wikipedia.org/wiki/"+curr.title;
+                var title = document.createElement("h4");
+                var link= document.createElement("a");
+                title.textContent=curr.title;
+                link.setAttribute("href", href);
+                link.appendChild(title);
+                link.className='title';
+                console.log(link);
+                var snippet = document.createElement("p");
+                snippet.innerHTML=curr.snippet;
+                searchItem.appendChild(link);
+                searchItem.appendChild(snippet);
+                //console.log(searchItem);
+                display.appendChild(searchItem);
 
-  })
+            })
+        },
+        error: function() {
+            console.log("error");
+        }
+
+    });
 }
-var submitBtn= document.getElementById("submitBtn");
-if(submitBtn){
-  console.log(found);
+function goToRandom(){
+  window.open("https://en.wikipedia.org/wiki/Special:Random","wiki_page");
 }
+var submitBtn = document.getElementById("submitBtn");
+
+var randomBtn= document.getElementById("randomBtn");
+var randomAction= randomBtn.addEventListener("click",goToRandom);
 var submitAction = submitBtn.addEventListener("click", getRequest);
