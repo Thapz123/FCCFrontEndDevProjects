f99f3093e9770166f4975ee5b97307b91858d271
Thapelo <Thapz.sebolai@gmail.com>
2016-12-01 17:37:57 -0800

    Completed most of js. Clear function is still iffy

--- calculator-fcc/js/script.js
+++ calculator-fcc/js/script.js
@@ -1,57 +1,96 @@
 $(document).ready(function() {
   var numpad = document.getElementById("numpad");
   var display = $("#display");
-  var numpadClick = numpad.addEventListener("click", calc.doOperation.bind(calc), false);
+  var numpadClick = numpad.addEventListener("click", calc.handleClick.bind(calc), false);
   console.log("ran");
 
-})
+});
 var operators = {
-    '+': function(a, b=0) { return a + b },
+    '+': function(a, b=0) { return a + b; },
     '÷': function(a, b=1) { 
           if(b==0)b=1;
-          return a / b },
+          return a / b ;},
     'x': function(a, b=1) {
-        if(b==0)b=1;
-        return a * b },
-    '-': function(a, b=0) { return a - b },
-    '=': function(a, b=0) { return b}
+        
+        return a * b ;},
+    '-': function(a, b=0) { return a - b; },
+    '=': function(a, b=0) { return a;},
+    'C': function(a, b=0) { clear();  return 0;}
      // ...
 };
+function clear(){
+    calc.init=false;
+    calc.total=0;
+    calc.currNum=0;
+    calc.currOp="";
+    calc.currNumText="";
+    calc.prev="";
+    $("#display").innerText="";
+    
+}
+
 var calc = {
   init:false,
   total: 0,
   currNum:0,
-  currText:"",
-  vaildOp:true,
-  enteredNum:false,
+  currOp:"",
+  // currText:"",
+  currNumText:"",
   
-  currOp:"=",
-  reloadDisplay:function(op){
-    if(operators.hasOwnProperty(op)){
-      this.currNum= Number(display.innerText);
-      
+  prev:"",
+  doOperation:function(op){
+    if(!operators.hasOwnProperty(op)){
+
+      if(!isNaN(op)){
+        // this.currText=this.currText+op;
+
+        if(this.prev=="eq"){
+          display.innerText="";
+          // this.currText="";
+          this.currNumText="";
+        }
+        this.currNumText= this.currNumText+op;
+        display.innerText=this.currNumText;
+        this.prev="num";
+      }
+      //console.log("num: ", this.currText);
+
+
+    }else{
+      this.currText=this.currText+op;
       if(!this.init){
-          this.init= true;
-          this.total= operators[op](total,currNum);
-          this.currNum=0;
-          
+        this.total= Number(this.currNumText);
+        this.currOp=op;
+        display.innerText="";
+        this.currNumText="";
+        this.init=true;
+      }else{
+        if(this.prev=="num"){
+          this.currNum=Number(this.currNumText);
+        }
+        this.currNumText="";
+        // console.log("totalB: ", this.total);
+        // console.log("currNumB: ",this.currNum);
+        this.total= operators[this.currOp](this.total,this.currNum);
+
+        // console.log("totalA: ", this.total);
+        // console.log("currNumA: ",this.currNum);
+        display.innerText=this.total;
+        this.currOp= op;
+        
+        if(op=='='){
+          this.prev="eq";
+        }
       }
-      if(op=="="&&this.currOp!="="){
-        this.total= operators[op](total,currNum);
-      }
-      
+      // console.log("op: ",this.currText);
     }
   },
   
-  doOperation: function(e) {
+  handleClick: function(e) {
     if (e.target !== e.currentTarget) {
       var operation = e.target.innerText;
-      this.reloadDisplay(operation);
-      console.log(operation);
-      var currText = "";
-      isNaN(operation) ? currText = operation : currText = display.innerText + operation;
-      display.innerHTML = currText;
+      this.doOperation(operation);
     }
     e.stopPropagation();
   }
-}
+};
